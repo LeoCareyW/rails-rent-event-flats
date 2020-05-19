5d99b45032c408cac9a86fb0092da746d7894e79
Mikey Ettlinger <mikeyettlinger@gmail.com>
2020-05-19 11:35:23 +0100

    preliminary pages show and flat

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "creating a flat"
+
+user1 = User.create!(
+  email: "mikey@gmail.com",
+  password: "123456",
+  password_confirmation: "123456")
+
+newflat = Flat.new(
+  name: "Nice Party House",
+  address: "A good location",
+  description: "It's fun",
+  price: 10)
+
+newflat.user = user1
+newflat.save
+
+puts "done"
+puts newflat
+
+
