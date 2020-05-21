e6f9c8de017e3ea78b6784f946687e65f9bd3347
Bradley Petty-Fitzmaurice <bradpfm@gmail.com>
2020-05-21 15:03:17 +0100

    Implemented Search Functionality

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -5,4 +5,10 @@ class Flat < ApplicationRecord
   has_many :bookings
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
+  include PgSearch::Model
+  pg_search_scope :search_by_address,
+  against: [ :address ],
+  using: {
+    tsearch: { prefix: true } 
+  }
 end
