851180dcc0783ca221bf964ed4ab50472be2e74d
Leo Carey-Williams <leocareywilliams@gmail.com>
2020-05-21 15:18:01 +0100

    review system complete

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,5 @@ class Review < ApplicationRecord
   belongs_to :flat
 
   validates :content, presence: true
+  validates :rating, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_21_095042) do
+ActiveRecord::Schema.define(version: 2020_05_21_134123) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -71,6 +71,8 @@ ActiveRecord::Schema.define(version: 2020_05_21_095042) do
     t.bigint "flat_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "reviews"
+    t.integer "rating"
     t.index ["flat_id"], name: "index_reviews_on_flat_id"
   end
 
