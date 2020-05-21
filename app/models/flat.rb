2ab1a96a840eaa79eed1b86f352ed4344a3e9737
Leo Carey-Williams <leocareywilliams@gmail.com>
2020-05-21 13:08:34 +0100

    reviews system and edit flat only available for user

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -3,6 +3,7 @@ class Flat < ApplicationRecord
   validates :photos, :name, :address, :price, presence: true
   belongs_to :user
   has_many :bookings
+  has_many :reviews, dependent: :destroy
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
 end
